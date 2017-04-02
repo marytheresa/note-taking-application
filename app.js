f2ff1a3c4764cbadb904cd94e4aa9e6a4dc4397c
Mary-Theresa <mtnwankwo@gmail.com>
2017-04-02 17:58:36 +0100

    Make the all the functions run from the command-line

--- app.js
+++ app.js
@@ -1,89 +1,133 @@
-
+#!/usr/bin/env node
 
 //console application to create notes
 
 var readline       = require('readline'),
     fs             = require('fs'),
+    program        = require('commander'),
+    findInFiles    = require('find-in-files'),
     filename       = "",
+    path           = "",
     note           = "",
-    directory      = "";
+    dir            = "./notes";
 
+program
+  .version('0.0.1')
+  .option('-c, --createnote <note_content>', 'Create a note')
+  .option('-v, --viewnote', 'View a single note')
+  .option('-d, --deletenote', 'Delete a single note')
+  .option('-l, --listnotes', 'View a formatted list of all the notes taken')
+  .option('-s, --searchnotes', 'View a formatted list of all the notes that can be identified by the query string')
+  .parse(process.argv);
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('Enter a name for your note here ==>  ', (name) => {
-    filename += name;
-    console.log(filename);
-    createFile(filename);
-})
+if (program.createnote) {
+    createnotes();
+};
 
-//function to check if file exists and create the file
+if (program.viewnote) {
+    readNote(path);
+};
 
-function createFile(filename) {
-    fs.open(filename, 'wx', (err, fd) => {
-        if (err) {
-            if (err.code === "EEXIST") {
-                console.error('file already exists');
-                return;
-            }
-            else {
-                throw err;
-            }
+if (program.deletenote) {
+    deleteNote(path);
+};
+
+if (program.listnotes) {
+    listNotes();
+};
+
+if (program.searchnotes) {
+    searchNotes(queryString);
+};
+
+function createnotes() {
+    function createFolder(dir) {
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
         }
-        console.log(fd)
-        writeNote(fd);
+    }
+
+    rl.question('Enter a name for your note here ==>  ', (filename) => {
+        createFile(filename);
     })
-}
 
-//function to write the notes
+    //function to check if file exists and create the file
 
-function writeNote(fd) {
-    rl.question('Enter your note here ==>  ', (data) => {
-        console.log(data)
-        note += data;
-        saveNote(fd, note);
-    });
-}
+    function createFile(filename) {
+        path = dir + '/' + filename;
+        fs.open(path, 'wx', (err, fd) => {
+            if (err) {
+                if (err.code === "EEXIST") {
+                    console.error('file already exists');
+                    return;
+                }
+                else {
+                    throw err;
+                }
+            }
+            writeNote(fd);
+        })
+    }
+
+    //function to write the notes
+
+    function writeNote(fd) {
+        rl.question('Enter your note here ==>  ', (note_content) => {
+            saveNote(fd, note_content);
+        });
+    }
     
-function saveNote(fd, note) {
-   fs.writeFile(fd, note, (err) => {
-        if (err) throw err;
-        console.log('Your note is saved!')
-        readNote(filename);
-    }) 
+    function saveNote(fd, note_content) {
+        fs.writeFile(fd, note_content, (err) => {
+            if (err) throw err;
+            console.log('Your note is saved!');
+        }) 
+    }
 }
  
-function readNote(filename) {
+function readNote(path) {
     rl.question('Enter the filename you want to read here ==>  ', (filename) => {
-        fs.readFile(filename, 'utf8', (err, data) => {
+        path = dir + '/' + filename;
+        fs.readFile(path, 'utf8', (err, data) => {
             if (err) throw err;
-            console.log(data)
-            deleteNote(filename);
+            console.log(data);
         });    
     });
 }
 
-function deleteNote(filename) {
+function deleteNote(path) {
     rl.question('Enter the filename you want to delete here ==>  ', (filename) => {
-        fs.unlink(filename, (err) => {
+        path = dir + '/' + filename;
+        fs.unlink(path, (err) => {
             if (err) throw err;
             console.log("Your note has been successfully deleted")
-            listNotes(directory)
-        });    
+        }); 
+        rl.close(); 
     });
 }
 
-function listNotes(directory) {
-    rl.question('Enter the directory from which you want to list the notes ==>  ', (directory) => {
-        fs.readdir(directory, (err, files) => {
-            if (err) throw err;
-            for (i = 0; i < files.length; i++) {
-                console.log(i + 1 + ". " + files[i])
-            }
-            rl.close();
-        });    
+function listNotes() {
+    fs.readdir('notes', (err, files) => {
+        if (err) throw err;
+        for (i = 0; i < files.length; i++) {
+            console.log(i + 1 + ". " + files[i])
+        }
+    });   
+}
+
+function searchNotes(queryString) {
+    findInFiles.find(queryString, './notes')
+    .then(function(results) {
+        var i = 1;
+        console.log('----------------"' + queryString + '" is found in the following files----------------')
+        for (result in results) {
+            console.log(i + ". " + result);
+            i += 1;
+        }
     });
 }
